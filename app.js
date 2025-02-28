116127aa57606b9bdb4b130517244c961c13aa80
Shreyas Anil Patil <shreyaspatil2468@gmail.com>
2025-02-28 18:40:00 +0530

    Updating errors in edit.ejs and added review model

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const path = require("path");
 const methodOverride=require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { error } = require("console");
+const {listingSchema}=require("./schmea.js");
+
+const Review = require("../Major Project/models/review.js")
 
 const port = 8080;
 
@@ -34,11 +39,20 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 });
 
+const validateListing=(res,req,next)=>{
+    let {error} = listingSchema.validate(req.body);
+        if(error){
+            throw new ExpressError(400,error); 
+        }else{
+            next();
+        }
+}
+
 //Index Route
-app.get("/Listings",async(req,res)=>{
+app.get("/Listings",wrapAsync(async(req,res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
-}); 
+})); 
 
 //New Route
 app.get("/listings/new",(req,res)=>{
@@ -46,41 +60,67 @@ app.get("/listings/new",(req,res)=>{
 });
 
 //Show Route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-});
+}));
 
 //create Route
-app.post("/listings",
+app.post(
+    "/listings",validateListing,
     wrapAsync(async(req,res,next)=>{
+        
         const newlisting = new Listing(req.body.listing);
         await newlisting.save();
         res.redirect("/listings");  
 }));
 
 //Edit Route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req,res)=>{
+app.put("/listings/:id",
+    validateListing, 
+    wrapAsync( async (req,res)=>{
     let{id}=req.params;
+    console.log(req.body)
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id", async(req,res)=>{
+app.delete("/listings/:id", wrapAsync(async(req,res)=>{
     let{id}=req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-})
+}));
+
+app.post("/listings/:id/reviews",async(req,res)=>{
+   let listing = await Listing.findById(req.params.id);
+   let newReview = await Review.create({
+    comment: req.body.comment,
+    rating: req.body.rating,
+   });
+   listing.reviews.push(newReview._id);
+   await listing.save();
+
+    res.redirect(`/success`);
+
+//    res.send("new review saved");
+});
+
+app.get("/success",(req,res)=>{
+    let message = "Added review"
+    res.render("./success.ejs",{message});
+});
+
+
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListings = new Listing({
 //         title:"My new Villa",
@@ -94,10 +134,17 @@ app.delete("/listings/:id", async(req,res)=>{
 //     res.send("Successful Testing");
 // });
 
+app.all("*", (req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"));
+});
+
 app.use((err,req,res,next)=>{
-    res.send("something went wrong");
-})
+    console.log(err)
+    let {statusCode=500,message=" Something went wrongDarshan"}=err;
+    res.status(statusCode).render("./error.ejs",{message})
+   // res.status(statusCode).send(message);
+});
 
 app.listen(port,()=>{
     console.log(`Port ${port} is started`);
-})
+});
--- models/listing.js
+++ models/listing.js
@@ -18,19 +18,28 @@ const listingSchema = new Schema({
             set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1723983556109-7415d601c377?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
         }
     },
-    price: {
-        type: Number,
-        required: true,
-        min: 0,
-    },
-    location: {
-        type: String,
-        required: true,
-    },
-    country: {
-        type: String,
-        required: true,
-    },
+    price:Number,
+    location:String,
+    country:String,
+    // price: {
+    //     type: Number,
+    //     required: true,
+    //     min: 0,
+    // },
+    // location: {
+    //     type: String,
+    //     required: true,
+    // },
+    // country: {
+    //     type: String,
+    //     required: true,
+    // },
+    reviews:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:"Review",
+        },
+    ],
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
